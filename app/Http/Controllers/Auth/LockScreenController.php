8a8d5c3bb8fd0255763a07cae43e34fa376be3d5
El Hadj Babacar Cissé <ehbc221@gmail.com>
2018-01-30 18:01:21 +0000

    updated lockScreen

--- app/Http/Controllers/Auth/LockScreenController.php
+++ app/Http/Controllers/Auth/LockScreenController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Input;
@@ -13,16 +12,16 @@ class LockScreenController extends Controller
 {
     public function get()
     {
+        $error = (session('error')) ? session('error') : null;
+
         if (Auth::check()) {
             Session::put('locked', true);
 
-            return view('auth.lock');
+            return view('auth.lock')
+                ->with('error', $error);
         }
 
-        $error = (session('error')) ? session('error') : null;
-
-        return view(route('login'))
-            ->with('error', $error);
+        return view(route('login'));
     }
 
     public function post()
@@ -35,7 +34,12 @@ class LockScreenController extends Controller
 
         if (Hash::check($password, Auth::user()->password)) {
             Session::forget('locked');
-            return redirect(route('accueil'));
+
+            if ($user = (Auth::user())->role->name === 'admin'){
+                return redirect()->intended(route('admin.dashboard'));
+            }
+
+            return redirect()->intended(route('student.dashboard'));
         }
         return redirect()->route('lock.get')
             ->with('error', 'Erreur d\'authentification');
